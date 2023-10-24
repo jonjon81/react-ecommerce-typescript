5a434619887cee0da94d019a9abe80a4446f84aa
Jon Atlas <jjjatlas@hotmail.com>
2023-10-24 15:48:40 +0100

    ts updates

---
+++ functions/create-payment-intent.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_KEY);
+exports.handler = async function (event, context) {
+  const { cart, shipping_fee, total_amount } = JSON.parse(event.body);
+
+  const calculateOrderAmount = () => {
+    // Replace this constant with a calculation of the order's amount
+    // Calculate the order total on the server to prevent
+    // people from directly manipulating the amount on the client
+    if (total_amount > 50) {
+      let newTotalAmount = 0.0;
+      newTotalAmount = total_amount.toFixed(2);
+      return newTotalAmount;
+    } else {
+      return shipping_fee + total_amount;
+    }
+  };
+  try {
+    // Create a PaymentIntent with the order amount and currency
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: calculateOrderAmount() * 100,
+      currency: 'usd',
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
